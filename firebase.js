27204bc9dd1602ff8dbfa2415cb284a9ebdb88a5
tratzy <naswacerih@gmail.com>
2026-05-02 10:15:55 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,37 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    signInWithPopup, 
+    GoogleAuthProvider, 
+    onAuthStateChanged, 
+    signOut 
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+// Ganti dengan konfigurasi Firebase Anda
+const firebaseConfig = {
+  apiKey: "AIzaSyBEPnglAGgeJxho9kfdM5d1Bhwlq-9dGDw",
+  authDomain: "nexstore-f286a.firebaseapp.com",
+  projectId: "nexstore-f286a",
+  storageBucket: "nexstore-f286a.appspot.com",
+  messagingSenderId: "604566007255",
+  appId: "1:604566007255:web:878da0f8b29e",
+  measurementId: "G-SF885JXHLV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { 
+    auth, 
+    googleProvider, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    signInWithPopup, 
+    onAuthStateChanged, 
+    signOut 
+};
